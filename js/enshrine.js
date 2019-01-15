e49c2676373e509eddd26736082b0c7c81fbb004
kaixuan1314 <1197827508@qq,com>
2019-01-15 14:17:04 +0800

    删除文件

---
+++ js/enshrine.js
@@ -0,0 +1,28 @@
+var app = getApp();
+/**
+ * 收藏
+ */
+function enshrine(result) {
+  var userId = wx.getStorageSync('userid');
+  var tId = wx.getStorageSync('tId');
+  var tType = wx.getStorageSync('tType');
+  var src = app.globalData.src + "/enshrine/addEnshrine";
+  wx.request({
+    url: src,
+    method: 'POST',
+    header: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: {
+      hId: tId,
+      tType: tType,
+      pId: userId
+    },
+    success(res) {
+      result(res);
+    }
+  })
+}
+
+//转化成小程序模板语言 这一步非常重要 不然无法正确调用
+module.exports = {
+  enshrine: enshrine
+};
