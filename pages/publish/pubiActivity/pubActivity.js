0de9696a13363e70aefc4c4fa280beaf4bf3318a
unknown <unknown@example.com>
2019-01-19 13:18:02 +0800

    lsy0119

--- pages/publish/pubiActivity/pubActivity.js
+++ pages/publish/pubiActivity/pubActivity.js
@@ -1,9 +1,35 @@
 // pages/publish/pubiActivity/pubActivity.js
 Page({
   data: {
-    imgShow:true
+    imgShow:true,
+    startdate: "开始日期",
+    starttime:"开始时间",
+    enddate: "结束日期",
+    endtime: "结束时间"
   },
   onLoad: function (options) {
 
+  },
+  startDateChange(e) {
+    this.setData({
+      startdate: e.detail.value
+    });
+    this.bindTimeChange()
+  },
+  startTimeChange(e) {
+    this.setData({
+      starttime: e.detail.value
+    })
+  },
+  endDateChange(e) {
+    this.setData({
+      enddate: e.detail.value
+    });
+    this.bindTimeChange()
+  },
+  endTimeChange(e) {
+    this.setData({
+      endtime: e.detail.value
+    })
   }
 })
