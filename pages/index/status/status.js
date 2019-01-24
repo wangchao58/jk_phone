ab2774130fd47e885e4b9270d690d29d4e1c1542
unknown <unknown@example.com>
2019-01-24 14:17:34 +0800

    lsy0124

--- pages/index/status/status.js
+++ pages/index/status/status.js
@@ -2,9 +2,14 @@
 Page({
   data: {
     iconType: 'success',
-    iconColor: 'green'
+    iconColor: 'green',
+    statusName:"报名成功",
+    statusTxt: "请准时参加活动哦"
   },
   onLoad: function (options) {
-
+     this.setData({
+       statusName:options.statusName,
+       statusTxt: options.statusTxt
+     })
   }
 })
