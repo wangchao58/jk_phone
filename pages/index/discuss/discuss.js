ffae1b6d36d9f6af847d1b0e2aa59530f8b2e2d9
unknown <unknown@example.com>
2019-01-30 16:55:37 +0800

    lsy0130

--- pages/index/discuss/discuss.js
+++ pages/index/discuss/discuss.js
@@ -64,7 +64,16 @@ Page({
       tContent: e.detail.value
     })
   },
+  imgck: function (event) {
+    var src = event.currentTarget.dataset.src;//获取data-src
+    var imgList = event.currentTarget.dataset.list;//获取data-list
+    //图片预览
+    wx.previewImage({
+      current: src, // 当前显示图片的http链接
+      urls: imgList // 需要预览的图片http链接列表
 
+    })
+  },
   /**
    * 资讯评论提交
    */
