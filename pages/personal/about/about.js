1924fa1380cab1561aa3420a096e3bfa3db9acf7
kaixuan1314 <1197827508@qq,com>
2019-02-25 16:50:57 +0800

    关于我们

--- pages/personal/about/about.js
+++ pages/personal/about/about.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    phone:"",
-    email:"",
-    topimg:"",
-    tab: [{ name:"欣星尚", active: "active", cont: false },
-      { name: "经营模式", active: "", cont:true}, 
-      { name: "加入我们", active: "", cont:true}],
-    employ:[]
-      
+    phone: "",
+    email: "",
+    topimg: "",
+    tab: [{ name: "小鲸通", active: "active", cont: false },
+    { name: "经营模式", active: "", cont: true },
+    { name: "加入我们", active: "", cont: true }],
+    employ: []
+
   },
   /**
    * 生命周期函数--监听页面加载
@@ -22,10 +22,10 @@ Page({
     this.employ();
   },
   //联系方式等信息
-  about:function(){
+  about: function () {
     var that = this;
     var src = app.globalData.src + "/trecruitInfo/trecruitInfoData";
-    
+
     wx.request({
       url: src,
       method: 'POST',
@@ -40,15 +40,15 @@ Page({
     })
   },
   //招聘信息
-  employ:function(){
-    var that=this;
+  employ: function () {
+    var that = this;
     var src = app.globalData.src + "/recruit/listRecruitByPort";
     wx.request({
       url: src,
       method: 'POST',
       success(res) {
-        for (var i = 0; i < res.data.length;i++){
-          var name="employ["+i+"].name";
+        for (var i = 0; i < res.data.length; i++) {
+          var name = "employ[" + i + "].name";
           var time = "employ[" + i + "].time";
           var address = "employ[" + i + "].address";
           var salary = "employ[" + i + "].salary";
@@ -63,27 +63,27 @@ Page({
       }
     })
   },
-  tab:function(e){
-    var that=this;
+  tab: function (e) {
+    var that = this;
     var id = e.target.id;
-    for (var i = 0;i<that.data.tab.length;i++){
-      var active = "tab["+i+"].active";
-      var cont = "tab["+i+"].cont";
-      if (that.data.tab[i].name==id){
-         that.setData({
-           [active]:"active",
-           [cont]:false
-           });
-       }else{
+    for (var i = 0; i < that.data.tab.length; i++) {
+      var active = "tab[" + i + "].active";
+      var cont = "tab[" + i + "].cont";
+      if (that.data.tab[i].name == id) {
+        that.setData({
+          [active]: "active",
+          [cont]: false
+        });
+      } else {
         that.setData({
           [active]: "",
           [cont]: true
         });
-       }
+      }
     }
   },
-  join:function(){
-    var email=this.data.email;
+  join: function () {
+    var email = this.data.email;
     var url = '../join/join?email=' + email
     wx.navigateTo({
       url: url
@@ -93,48 +93,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
